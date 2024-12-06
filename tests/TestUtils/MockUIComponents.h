893a8e09195146d87f1576445de2a836d563da8a
Tchakam <yvantchakam@gmail.com>
2024-12-06 10:49:58 +0100

    creation d'un repertoire pour les tests

---
+++ tests/TestUtils/MockUIComponents.h
@@ -0,0 +1,15 @@
+// MainWindow.h
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+
+class MainWindow : public QMainWindow {
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+};
+
+#endif
